f1b9d70f1b6c61c7c224b2067638d3dc9e8fddfc
Shubham Waje <waje.shubham111@gmail.com>
2023-05-29 18:50:19 +0530

    Remove length wise validation in password

--- src/models/apps/auth/user.models.js
+++ src/models/apps/auth/user.models.js
@@ -28,7 +28,6 @@ const userSchema = new Schema(
     password: {
       type: String,
       required: [true, "Password is required"],
-      minlength: [8, "Password must be at least 8 characters"],
     },
     isEmailVerified: {
       type: Boolean,
