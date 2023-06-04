a6b30c0d3704c670220b7ce257f6433557441883
Shubham Waje <waje.shubham111@gmail.com>
2023-06-04 12:51:10 +0530

    Configure disk storage for multer to save file in respective format instead of binary

--- src/middlewares/multer.middlewares.js
+++ src/middlewares/multer.middlewares.js
@@ -1,6 +1,24 @@
 import multer from "multer";
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    // This storage needs public/images folder in the root directory
+    // Else it will throw an error saying cannot find path public/images
+    cb(null, "./public/images");
+  },
+  // Store file in a .png/.jpeg/.jpg format instead of binary
+  filename: function (req, file, cb) {
+    let fileExtension = "";
+    if (file.originalname.split(".").length > 1) {
+      fileExtension = file.originalname.substring(
+        file.originalname.lastIndexOf(".")
+      );
+    }
+    cb(null, file.fieldname + "-" + Date.now() + fileExtension);
+  },
+});
+
 // Middleware responsible to read form data and upload the File object to the mentioned path
 export const upload = multer({
-  dest: "public/images",
+  storage,
 });
