7945079cd12c74d93114f2a92e114ee502e605f7
Shubham Waje <waje.shubham111@gmail.com>
2023-06-08 18:51:00 +0530

    Remove comment from app.js

--- src/app.js
+++ src/app.js
@@ -21,7 +21,6 @@ app.use(
   })
 );
 
-// TODO: resolve issue with parsing the form data
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public")); // configure static file to save images locally
