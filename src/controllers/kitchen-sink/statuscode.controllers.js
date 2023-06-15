961f5c446a504f178fdebcc8142da30199d62539
Shubham Waje <waje.shubham111@gmail.com>
2023-06-15 23:24:14 +0530

    Add get all status codes api

--- src/controllers/kitchen-sink/statuscode.controllers.js
+++ src/controllers/kitchen-sink/statuscode.controllers.js
@@ -35,4 +35,10 @@ const getStatusCode = asyncHandler(async (req, res) => {
     );
 });
 
-export { getStatusCode };
+const getAllStatusCodes = asyncHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json(new ApiResponse(200, statusCodesJson, "Status codes fetched"));
+});
+
+export { getStatusCode, getAllStatusCodes };
--- src/routes/kitchen-sink/statuscode.routes.js
+++ src/routes/kitchen-sink/statuscode.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { getStatusCode } from "../../controllers/kitchen-sink/statuscode.controllers.js";
+import {
+  getAllStatusCodes,
+  getStatusCode,
+} from "../../controllers/kitchen-sink/statuscode.controllers.js";
 import { statusCodeValidator } from "../../validators/kitchen-sink/statuscode.validators.js";
 import { validate } from "../../validators/validate.js";
 
 const router = Router();
+
+router.route("/").get(getAllStatusCodes);
 
 router
   .route("/:statusCode")
