b059ab5d9b39877606ce8ee7d069c9e18c628618
Shubham Waje <waje.shubham111@gmail.com>
2023-06-22 22:58:36 +0530

    Add isFollowing flag in get followers and following list

--- src/controllers/apps/social-media/follow.controllers.js
+++ src/controllers/apps/social-media/follow.controllers.js
@@ -59,8 +59,6 @@ const followUnFollowUser = asyncHandler(async (req, res) => {
   }
 });
 
-// TODO: Add isFollowing flag in the followers list. So frontend can show follow/unfollow button based on that flag in the user's follow list. ONLY IF USER IS SEEING HIS?HER OWN FOLLOWER'S LIST
-// TODO: IMP: CREATE ECOM AND SOCIAL MEDIA PROFILES ON AUTH REGISTRATION TO AVOID MISSING PROFILES IN MODULES
 const getFollowersListByUserName = asyncHandler(async (req, res) => {
   const { username } = req.params;
   const user = await User.findOne({ username });
@@ -95,7 +93,49 @@ const getFollowersListByUserName = asyncHandler(async (req, res) => {
               as: "profile",
             },
           },
-          { $addFields: { profile: { $first: "$profile" } } },
+          {
+            // NOTE: In this logic we want to treat logged in user as a follower
+            // LOGIC TO CHECK IF THE LOGGED IN USER IS FOLLOWING ANY OF THE FOLLOWERS
+            // Point to be noted: There are chances that the logged in user is seeing someone else's follower list
+            $lookup: {
+              // We want to check if there is a document where follower is current logged in user and followee is the looked up user
+              // If there is a document with above case that means logged in user is following the looked up user
+              from: "socialfollows",
+              localField: "_id", // ID of the looked up user
+              foreignField: "followeeId",
+              as: "isFollowing",
+              pipeline: [
+                {
+                  $match: {
+                    followerId: new mongoose.Types.ObjectId(req.user?._id), // Only get documents where logged in user is the follower
+                  },
+                },
+              ],
+            },
+          },
+
+          // Now we wither get no document (meaning, logged in user is not following anyone) or have the document where `LOOKED UP USER is the one who is BEING FOLLOWED BY THE CURRENT LOGGED IN USER`
+          // So, if the document exist then the isFollowing flag should be true
+          {
+            $addFields: {
+              profile: { $first: "$profile" },
+              isFollowing: {
+                $cond: {
+                  if: {
+                    $gte: [
+                      {
+                        // if the isFollowing key has document in it
+                        $size: "$isFollowing",
+                      },
+                      1,
+                    ],
+                  },
+                  then: true,
+                  else: false,
+                },
+              },
+            },
+          },
           {
             $project: {
               // only project necessary fields
@@ -103,6 +143,7 @@ const getFollowersListByUserName = asyncHandler(async (req, res) => {
               email: 1,
               avatar: 1,
               profile: 1,
+              isFollowing: 1,
             },
           },
         ],
@@ -204,7 +245,45 @@ const getFollowingListByUserName = asyncHandler(async (req, res) => {
               as: "profile",
             },
           },
-          { $addFields: { profile: { $first: "$profile" } } },
+          // NOTE: In this logic we want to treat logged in user as a follower
+          // LOGIC TO CHECK IF THE LOGGED IN USER IS FOLLOWING ANY OF THE USERS THAT LOADED PROFILE USER FOLLOWING
+          // Point to be noted: There are chances that the logged in user is seeing someone else's following list. SO if logged in user is seeing his own following list the isFollowing flag will be true
+          {
+            $lookup: {
+              // We want to check if there is a document where follower is current logged in user and followee is the looked up user
+              // If there is a document with above case that means logged in user is following the looked up user
+              from: "socialfollows",
+              localField: "_id",
+              foreignField: "followeeId",
+              as: "isFollowing",
+              pipeline: [
+                {
+                  $match: {
+                    followerId: new mongoose.Types.ObjectId(req.user?._id), // Only get documents where logged in user is the follower
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              profile: { $first: "$profile" },
+              isFollowing: {
+                $cond: {
+                  if: {
+                    $gte: [
+                      {
+                        $size: "$isFollowing",
+                      },
+                      1,
+                    ],
+                  },
+                  then: true,
+                  else: false,
+                },
+              },
+            },
+          },
           {
             $project: {
               // only project necessary fields
@@ -212,6 +291,7 @@ const getFollowingListByUserName = asyncHandler(async (req, res) => {
               email: 1,
               avatar: 1,
               profile: 1,
+              isFollowing: 1,
             },
           },
         ],
--- src/routes/apps/social-media/follow.routes.js
+++ src/routes/apps/social-media/follow.routes.js
@@ -4,7 +4,10 @@ import {
   getFollowersListByUserName,
   getFollowingListByUserName,
 } from "../../../controllers/apps/social-media/follow.controllers.js";
-import { verifyJWT } from "../../../middlewares/auth.middlewares.js";
+import {
+  getLoggedInUserOrIgnore,
+  verifyJWT,
+} from "../../../middlewares/auth.middlewares.js";
 import { toBeFollowedUserIdValidator } from "../../../validators/apps/social-media/follow.validators.js";
 import { validate } from "../../../validators/validate.js";
 
@@ -14,8 +17,12 @@ router
   .route("/:toBeFollowedUserId")
   .post(verifyJWT, toBeFollowedUserIdValidator(), validate, followUnFollowUser);
 
-router.route("/list/followers/:username").get(getFollowersListByUserName);
+router
+  .route("/list/followers/:username")
+  .get(getLoggedInUserOrIgnore, getFollowersListByUserName);
 
-router.route("/list/following/:username").get(getFollowingListByUserName);
+router
+  .route("/list/following/:username")
+  .get(getLoggedInUserOrIgnore, getFollowingListByUserName);
 
 export default router;
