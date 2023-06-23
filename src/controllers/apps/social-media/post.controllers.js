d4e128689db83c96f1b904fdf945186cbc7b79bb
Shubham Waje <waje.shubham111@gmail.com>
2023-06-23 12:49:05 +0530

    Add post endpoints - Add get all post endpoint - Add get post by id and delete post endpoint - Add a utility function returning mongoose aggregation pipeline to be reused to structure the social post

--- src/controllers/apps/social-media/post.controllers.js
+++ src/controllers/apps/social-media/post.controllers.js
@@ -1,7 +1,83 @@
+import mongoose from "mongoose";
 import { SocialPost } from "../../../models/apps/social-media/post.models.js";
 import { ApiResponse } from "../../../utils/ApiResponse.js";
 import { asyncHandler } from "../../../utils/asyncHandler.js";
 import { getLocalPath, getStaticFilePath } from "../../../utils/helpers.js";
+import { ApiError } from "../../../utils/ApiError.js";
+
+/**
+ * @description Utility function which returns the pipeline stages to structure the social post schema with calculations like, likes count, comments count, isLiked, isBookmarked etc
+ * @returns {mongoose.PipelineStage[]}
+ */
+const postCommonAggregation = () => {
+  return [
+    {
+      $lookup: {
+        from: "sociallikes",
+        localField: "_id",
+        foreignField: "postId",
+        as: "likes",
+      },
+    },
+    {
+      $lookup: {
+        from: "sociallikes",
+        localField: "author",
+        foreignField: "likedBy",
+        as: "isLiked",
+      },
+    },
+    {
+      $lookup: {
+        from: "socialprofiles",
+        localField: "author",
+        foreignField: "owner",
+        as: "author",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "account",
+              pipeline: [
+                {
+                  $project: {
+                    avatar: 1,
+                    email: 1,
+                    username: 1,
+                  },
+                },
+              ],
+            },
+          },
+          { $addFields: { account: { $first: "$account" } } },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        author: { $first: "$author" },
+        likes: { $size: "$likes" },
+        isLiked: {
+          $cond: {
+            if: {
+              $gte: [
+                {
+                  // if the isLiked key has document in it
+                  $size: "$isLiked",
+                },
+                1,
+              ],
+            },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+  ];
+};
 
 const createPost = asyncHandler(async (req, res) => {
   const { content, tags } = req.body;
@@ -33,4 +109,49 @@ const createPost = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, post, "Post created successfully"));
 });
 
-export { createPost };
+const getAllPosts = asyncHandler(async (req, res) => {
+  const posts = await SocialPost.aggregate([...postCommonAggregation()]);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, posts, "Posts fetched successfully"));
+});
+
+const getPostById = asyncHandler(async (req, res) => {
+  const { postId } = req.params;
+  const post = await SocialPost.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(postId),
+      },
+    },
+    ...postCommonAggregation(),
+  ]);
+
+  if (!post[0]) {
+    throw new ApiError(404, "Post does not exist");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, post[0], "Post fetched successfully"));
+});
+
+const deletePost = asyncHandler(async (req, res) => {
+  const { postId } = req.params;
+
+  const post = await SocialPost.findOneAndDelete({
+    _id: postId,
+    author: req.user._id,
+  });
+
+  if (!post) {
+    throw new ApiError(404, "Post does not exist");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Post deleted successfully"));
+});
+
+export { createPost, getAllPosts, getPostById, deletePost };
--- src/routes/apps/social-media/post.routes.js
+++ src/routes/apps/social-media/post.routes.js
@@ -1,14 +1,23 @@
 import { Router } from "express";
-import { createPost } from "../../../controllers/apps/social-media/post.controllers.js";
+import {
+  createPost,
+  deletePost,
+  getAllPosts,
+  getPostById,
+} from "../../../controllers/apps/social-media/post.controllers.js";
 import { verifyJWT } from "../../../middlewares/auth.middlewares.js";
 import { upload } from "../../../middlewares/multer.middlewares.js";
-import { createPostValidator } from "../../../validators/apps/social-media/post.validators.js";
+import {
+  createPostValidator,
+  postPathVariableValidator,
+} from "../../../validators/apps/social-media/post.validators.js";
 import { validate } from "../../../validators/validate.js";
 
 const router = Router();
 
 router
   .route("/")
+  .get(getAllPosts)
   .post(
     verifyJWT,
     upload.fields([{ name: "images", maxCount: 6 }]),
@@ -16,5 +25,10 @@ router
     validate,
     createPost
   );
+
+router
+  .route("/:postId")
+  .get(postPathVariableValidator(), validate, getPostById)
+  .delete(verifyJWT, postPathVariableValidator(), validate, deletePost);
 
 export default router;
