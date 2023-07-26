3dcde33eee4b2f2fdc66731693fd08e966ec3f88
Shubham Waje <waje.shubham111@gmail.com>
2023-07-27 02:01:19 +0530

    feat(chat-app): add chat models

---
+++ src/models/apps/chat-app/chat.models.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const chatSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    isGroupChat: {
+      type: Boolean,
+      default: false,
+    },
+    lastMessage: {
+      type: Schema.Types.ObjectId,
+      ref: "ChatMessage",
+    },
+    participants: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    admin: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+chatSchema.plugin(mongooseAggregatePaginate);
+
+export const Chat = mongoose.model("Chat", chatSchema);
---
+++ src/models/apps/chat-app/message.models.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+// TODO: Add image and pdf file sharing in next version
+const chatMessageSchema = new Schema(
+  {
+    sender: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    content: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    chatItem: {
+      type: Schema.Types.ObjectId,
+      ref: "Chat",
+    },
+  },
+  { timestamps: true }
+);
+
+chatMessageSchema.plugin(mongooseAggregatePaginate);
+
+export const ChatMessage = mongoose.model("ChatMessage", chatMessageSchema);
