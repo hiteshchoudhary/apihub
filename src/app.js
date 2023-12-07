39cb0053344d414b0e0a5540694662240808ca1e
Shubham Waje <waje.shubham111@gmail.com>
2023-12-07 12:28:20 +0530

    refactor(rate-limit): update IP rate limit to 5000 requests per window

--- src/app.js
+++ src/app.js
@@ -51,7 +51,7 @@ app.use(requestIp.mw());
 // Rate limiter to avoid misuse of the service and avoid cost spikes
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 500, // Limit each IP to 500 requests per `window` (here, per 15 minutes)
+  max: 5000, // Limit each IP to 500 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   keyGenerator: (req, res) => {
