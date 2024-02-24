d7b1f744f200f0aded1d4049c68603e547a5866a
atulbhatt-system32 <atulbhatt98@gmail.com>
2024-02-24 22:23:12 +0530

    feat: updated home page with PageContainer as a layout

--- examples/kitchen-sink/statuscodes/web/react-shadcn-tailwind-zustand/src/pages/home/Home.tsx
+++ examples/kitchen-sink/statuscodes/web/react-shadcn-tailwind-zustand/src/pages/home/Home.tsx
@@ -1,37 +1,37 @@
-import { Header } from "@/layout";
+import { PageContainer } from "@/layout";
 import { hero_illustrator, wave_vector } from "./assets";
 import { Button } from "@/components/ui/button";
 import "./home.css";
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-start h-screen">
-      <Header />
-      <main className="flex max-w-screen-lg px-4 pt-[8%]">
-        <div className="flex flex-col h-full">
-          <h2 className="text-6xl font-bold leading-[102px]">
-            Decoding HTTP with Status Code Insight
-          </h2>
-          <p className="mt-4 text-lg max-w-[517px]">
-            StatusCode Insight is your ultimate guide to understanding HTTP
-            status codes, offering a detailed, user-friendly interface for both
-            beginners and seasoned professionals in web development.
-          </p>
-          <div className="flex items-center w-full mt-8 space-x-8">
-            <Button className="text-black bg-white hover:bg-blue-500 hover:text-white">
-              Play Quiz
-            </Button>
-            <Button className="text-black bg-white hover:bg-blue-500 hover:text-white">
-              Check Codes
-            </Button>
-          </div>
+    <PageContainer
+      footer={
+        <img
+          src={wave_vector}
+          alt="wave_vector"
+          className="absolute bottom-0 w-full wave -left-4 right-0 z-[-1] min-w-[105vw]"
+        />
+      }
+    >
+      <div className="flex flex-col h-full">
+        <h2 className="text-6xl font-bold leading-[102px]">
+          Decoding HTTP with Status Code Insight
+        </h2>
+        <p className="mt-4 text-lg max-w-[517px]">
+          StatusCode Insight is your ultimate guide to understanding HTTP status
+          codes, offering a detailed, user-friendly interface for both beginners
+          and seasoned professionals in web development.
+        </p>
+        <div className="flex items-center w-full mt-8 space-x-8">
+          <Button className="text-black bg-white hover:bg-blue-500 hover:text-white">
+            Play Quiz
+          </Button>
+          <Button className="text-black bg-white hover:bg-blue-500 hover:text-white">
+            Check Codes
+          </Button>
         </div>
-        <img src={hero_illustrator} alt="hero_illustrator" />
-      </main>
-      <img
-        src={wave_vector}
-        alt="wave_vector"
-        className="absolute bottom-0 w-full wave -left-4 right-0 z-[-1] min-w-[105vw]"
-      />
-    </div>
+      </div>
+      <img src={hero_illustrator} alt="hero_illustrator" />
+    </PageContainer>
   );
 }
