ff89a9f6685962f1b3162235e9f42dd1cebf91ab
atulbhatt-system32 <atulbhatt98@gmail.com>
2024-02-24 22:22:10 +0530

    feat: logo isn ow clickable to redirect to homepage

--- examples/kitchen-sink/statuscodes/web/react-shadcn-tailwind-zustand/src/layout/header/Header.tsx
+++ examples/kitchen-sink/statuscodes/web/react-shadcn-tailwind-zustand/src/layout/header/Header.tsx
@@ -5,14 +5,15 @@ import {
 } from "@/components/ui/navigation-menu";
 import navigationMenuItems from "./menuItem";
 import { NavigationMenuLink } from "@radix-ui/react-navigation-menu";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <div className="flex justify-center w-full border-b-4 border-white">
       <div className="flex items-center justify-between w-full max-w-screen-lg p-4">
-        <a className="font-bold no-underline cursor-pointer">
+        <Link className="font-bold no-underline cursor-pointer" to="/">
           HTTP Status Insight
-        </a>
+        </Link>
         <NavigationMenu>
           <NavigationMenuList>
             <NavigationMenuItem className="space-x-4">
