ae88416666ff14714b1c10a5873a8039220fbd08
atulbhatt-system32 <atulbhatt98@gmail.com>
2024-02-24 22:23:37 +0530

    feat: added basic structure for other pages of the app

---
+++ examples/kitchen-sink/statuscodes/web/react-shadcn-tailwind-zustand/src/pages/findCode/FindCode.tsx
@@ -0,0 +1,5 @@
+import { PageContainer } from "@/layout";
+
+export default function FindCode() {
+  return <PageContainer>FindCode</PageContainer>;
+}
---
+++ examples/kitchen-sink/statuscodes/web/react-shadcn-tailwind-zustand/src/pages/quiz/Quiz.tsx
@@ -0,0 +1,5 @@
+import { PageContainer } from "@/layout";
+
+export default function Quiz() {
+  return <PageContainer>Quiz</PageContainer>;
+}
