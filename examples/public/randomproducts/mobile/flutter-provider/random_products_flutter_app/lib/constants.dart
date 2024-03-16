3f48842b19b1a24a97b1a5d077441122c6223b30
Sarthak Agarwal <57105611+Sarthak-ONS@users.noreply.github.com>
2024-03-16 22:39:58 +0530

    feat: minor changes

--- examples/public/randomproducts/mobile/flutter-provider/random_products_flutter_app/lib/constants.dart
+++ examples/public/randomproducts/mobile/flutter-provider/random_products_flutter_app/lib/constants.dart
@@ -6,6 +6,7 @@ const kCurrencySymbol = "₹";
 final kProductImageSliderOptions = CarouselOptions(
   aspectRatio: 16 / 9,
   viewportFraction: 0.75,
+  enlargeCenterPage: true,
   initialPage: 0,
   reverse: false,
   autoPlay: true,
@@ -13,4 +14,6 @@ final kProductImageSliderOptions = CarouselOptions(
   autoPlayAnimationDuration: const Duration(milliseconds: 800),
   autoPlayCurve: Curves.fastOutSlowIn,
   scrollDirection: Axis.horizontal,
+  animateToClosest: true,
+  scrollPhysics: const BouncingScrollPhysics(),
 );
