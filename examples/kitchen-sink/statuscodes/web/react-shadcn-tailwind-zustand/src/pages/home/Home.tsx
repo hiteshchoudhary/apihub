33eeb7d610b8e4b66ee4d0c06288962cc64cf0d9
atulbhatt-system32 <atulbhatt98@gmail.com>
2024-03-17 18:00:04 +0530

    feat: some responsive tweaks for home page

--- examples/kitchen-sink/statuscodes/web/react-shadcn-tailwind-zustand/src/pages/home/Home.tsx
+++ examples/kitchen-sink/statuscodes/web/react-shadcn-tailwind-zustand/src/pages/home/Home.tsx
@@ -11,33 +11,41 @@ export default function Home() {
         <img
           src={wave_vector}
           alt="wave_vector"
-          className="absolute bottom-0 w-full wave -left-4 right-0 z-[-1] min-w-[105vw]"
+          className="w-full wave -left-4 right-0 z-[-1] min-w-[105vw] absolute bottom-0 sm:hidden md:block"
         />
       }
     >
-      <div className="flex flex-col h-full">
-        <h2 className="text-6xl font-bold leading-[102px]">
-          Decoding HTTP with Status Code Insight
-        </h2>
-        <p className="mt-4 text-lg max-w-[517px]">
-          StatusCode Insight is your ultimate guide to understanding HTTP status
-          codes, offering a detailed, user-friendly interface for both beginners
-          and seasoned professionals in web development.
-        </p>
-        <div className="flex items-center w-full mt-8 space-x-8">
-          <Link to="/quiz">
-            <Button className="text-black bg-white hover:bg-blue-500 hover:text-white">
-              Play Quiz
-            </Button>
-          </Link>
-          <Link to="/codes-list">
-            <Button className="text-black bg-white hover:bg-blue-500 hover:text-white">
-              Check Codes
-            </Button>
-          </Link>
+      <div className="flex w-full h-full">
+        <div className="flex flex-col h-full">
+          <h2 className="text-6xl font-bold leading-[102px]">
+            Decoding HTTP with Status Code Insight
+          </h2>
+          <p className="mt-4 text-lg max-w-[517px]">
+            StatusCode Insight is your ultimate guide to understanding HTTP
+            status codes, offering a detailed, user-friendly interface for both
+            beginners and seasoned professionals in web development.
+          </p>
+          <div className="flex items-center w-full mt-8 space-x-8">
+            <Link to="/quiz">
+              <Button className="text-black bg-white hover:bg-blue-500 hover:text-white">
+                Play Quiz
+              </Button>
+            </Link>
+            <Link to="/codes-list">
+              <Button className="text-black bg-white hover:bg-blue-500 hover:text-white">
+                Check Codes
+              </Button>
+            </Link>
+          </div>
+        </div>
+        <div className="hidden lg:flex lg:items-center">
+          <img
+            src={hero_illustrator}
+            alt="hero_illustrator"
+            className="w-full h-full"
+          />
         </div>
       </div>
-      <img src={hero_illustrator} alt="hero_illustrator" />
     </PageContainer>
   );
 }
