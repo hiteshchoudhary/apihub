ff7b625c2036cbce1f220c17f14dcd854c59beb2
arnb-smnta <loveumearnab.2812000@gmail.com>
2024-03-20 21:18:15 +0530

    fix: correct a typo in constants.js

--- src/constants.js
+++ src/constants.js
@@ -68,7 +68,7 @@ export const USER_TEMPORARY_TOKEN_EXPIRY = 20 * 60 * 1000; // 20 minutes
 export const MAXIMUM_SUB_IMAGE_COUNT = 4;
 export const MAXIMUM_SOCIAL_POST_IMAGE_COUNT = 6;
 
-export const DB_NAME = "freeapi-3"; //"freeapi";
+export const DB_NAME = "freeapi";
 
 export const paypalBaseUrl = {
   sandbox: "https://api-m.sandbox.paypal.com",
