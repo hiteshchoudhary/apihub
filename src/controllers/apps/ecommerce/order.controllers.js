ca09d1e12486c7ffa5fc1e7a484cf8d8bb4c9b38
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2024-03-27 03:20:02 +0000

    fix: address not found after deleting it in orders, closes #106

--- src/controllers/apps/ecommerce/order.controllers.js
+++ src/controllers/apps/ecommerce/order.controllers.js
@@ -203,7 +203,6 @@ const generateRazorpayOrder = asyncHandler(async (req, res) => {
       // In case payment is done and there is some network issue in the payment verification api
       // We will at least have a record of the order
       const unpaidOrder = await EcomOrder.create({
-        address: addressId,
         address: {
           street: address.street,
           city: address.city,
--- src/models/apps/ecommerce/order.models.js
+++ src/models/apps/ecommerce/order.models.js
@@ -48,8 +48,6 @@ const orderSchema = new Schema(
       default: [],
     },
     address: {
-      type: Schema.Types.ObjectId,
-      ref: "Address",
       street: String,
       city: String,
       state: String,
