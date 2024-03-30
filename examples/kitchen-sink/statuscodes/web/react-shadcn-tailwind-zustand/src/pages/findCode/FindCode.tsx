5aa6e5b393c4aecffcadbac576d6c354829389eb
atulbhatt-system32 <atulbhatt98@gmail.com>
2024-03-30 16:43:57 +0530

    feat: added ombo-box

--- examples/kitchen-sink/statuscodes/web/react-shadcn-tailwind-zustand/src/pages/findCode/FindCode.tsx
+++ examples/kitchen-sink/statuscodes/web/react-shadcn-tailwind-zustand/src/pages/findCode/FindCode.tsx
@@ -1,41 +1,81 @@
 import { PageContainer } from "@/layout";
 
+import { Check, ChevronsUpDown } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
 import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { useStatusCodeStore } from "@/store/statusCodes.store";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 export default function FindCode() {
   const { HTTPStatusCodesList } = useStatusCodeStore((state) => state);
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+  const allCodes = [];
+  if (!HTTPStatusCodesList) return null;
+  for (const codeGroup in HTTPStatusCodesList) {
+    const codes = HTTPStatusCodesList[codeGroup].statusCodes;
+    const codeGroupCodes = codes.map((code) => ({
+      label: code.code,
+      code: code.description,
+    }));
+    allCodes.push(...codeGroupCodes);
+  }
+
   return (
     <PageContainer>
       <div className="flex flex-col items-center justify-center w-full gap-8">
-        <Select>
-          <SelectTrigger className="w-[280px]">
-            <SelectValue placeholder="Select a code" />
-          </SelectTrigger>
-          <SelectContent>
-            {Object.keys(HTTPStatusCodesList).map((key) => {
-              const { title, statusCodes } = HTTPStatusCodesList[key];
-              return (
-                <SelectGroup key={key}>
-                  <SelectLabel>{title}</SelectLabel>
-                  {statusCodes.map(({ code, message }) => (
-                    <SelectItem key={code} value={code}>
-                      {message}
-                    </SelectItem>
-                  ))}
-                </SelectGroup>
-              );
-            })}
-          </SelectContent>
-        </Select>
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Button
+              variant="outline"
+              role="combobox"
+              aria-expanded={open}
+              className="w-[200px] justify-between"
+            >
+              {value
+                ? allCodes.find((code) => code.code === value)?.label
+                : "Select framework..."}
+              <ChevronsUpDown className="w-4 h-4 ml-2 opacity-50 shrink-0" />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-[200px] p-0">
+            <Command>
+              <CommandInput placeholder="Search framework..." />
+              <CommandEmpty>No framework found.</CommandEmpty>
+              <CommandGroup>
+                {allCodes.map((code) => (
+                  <CommandItem
+                    key={code.label}
+                    onClick={() => {
+                      setValue(code.code);
+                      setOpen(false);
+                    }}
+                    className={cn(
+                      "flex items-center justify-between px-4 py-2",
+                      value === code.code && "bg-gray-100"
+                    )}
+                  >
+                    <span>{code.label}</span>
+                    {value === code.code && <Check />}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </Command>
+          </PopoverContent>
+        </Popover>
       </div>
     </PageContainer>
   );
