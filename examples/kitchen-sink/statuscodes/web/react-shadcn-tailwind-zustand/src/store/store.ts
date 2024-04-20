114115b5b0c61899243fddd80d209bca5adc3cd9
atulbhatt-system32 <atulbhatt98@gmail.com>
2024-04-20 17:17:42 +0530

    feat: persist store

--- examples/kitchen-sink/statuscodes/web/react-shadcn-tailwind-zustand/src/store/store.ts
+++ examples/kitchen-sink/statuscodes/web/react-shadcn-tailwind-zustand/src/store/store.ts
@@ -1,5 +1,6 @@
 import { StatusListType } from "@/pages/codesList/components/types";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface StoreType {
   HTTPStatusCodesList: StatusListType;
@@ -8,12 +9,20 @@ interface StoreType {
   setLoading: (loading: boolean) => void;
 }
 
-export const useAppStore = create<StoreType>()((set) => ({
-  HTTPStatusCodesList: {},
-  setHTTPStatusCodesList: (HTTPStatusCodesList) => set({ HTTPStatusCodesList }),
-  loading: 0,
-  setLoading: (loading) =>
-    set((state) => ({
-      loading: loading ? state.loading + 1 : state.loading - 1,
-    })),
-}));
+export const useAppStore = create<StoreType>()(
+  persist(
+    (set) => ({
+      HTTPStatusCodesList: {},
+      setHTTPStatusCodesList: (HTTPStatusCodesList) =>
+        set({ HTTPStatusCodesList }),
+      loading: 0,
+      setLoading: (loading) =>
+        set((state) => ({
+          loading: loading ? state.loading + 1 : state.loading - 1,
+        })),
+    }),
+    {
+      name: "HTTPStatusCodesList",
+    }
+  ) // Add an empty object as the second argument
+);
