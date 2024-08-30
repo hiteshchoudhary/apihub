42c90ebcd9403f1408ef6b08e3317a6e3a1575a4
Tejas Surse <118660087+TejasSurse@users.noreply.github.com>
2024-08-30 14:25:11 +0530

    Update todo.controllers.js
    The use of string interpolation directly inside the template literal ensures clarity and correctness.

--- src/controllers/apps/todo/todo.controllers.js
+++ src/controllers/apps/todo/todo.controllers.js
@@ -98,10 +98,11 @@ const toggleTodoDoneStatus = asyncHandler(async (req, res) => {
     .status(200)
     .json(
       new ApiResponse(
-        200,
-        todo,
-        "Todo marked " + todo.isComplete ? "done" : "undone"
-      )
+    200,
+    todo,
+    `Todo marked ${todo.isComplete ? "done" : "undone"}`
+    )
+
     );
 });
 
