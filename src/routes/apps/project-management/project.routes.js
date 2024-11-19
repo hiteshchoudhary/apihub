5ce66fc3c772c65ebc80bd178627eba35e37b60d
codemitrayt <codemitrayt@gmail.com>
2024-11-20 00:13:27 +0530

    feat: add task routes

--- src/routes/apps/project-management/project.routes.js
+++ src/routes/apps/project-management/project.routes.js
@@ -21,6 +21,20 @@ import {
   updateProject,
   acceptInvitation,
 } from "../../../controllers/apps/project-management/project.controllers.js";
+import {
+  assignMemberToTask,
+  createTask,
+  deleteTask,
+  getTasks,
+  removeMemberFromTask,
+  updateTask,
+} from "../../../controllers/apps/project-management/task.controllers.js";
+import {
+  assignMemberToTaskValidator,
+  deleteTaskValidator,
+  taskIdValidator,
+  taskValidator,
+} from "../../../validators/apps/project-management/task.validators.js";
 
 const router = Router();
 
@@ -43,6 +57,31 @@ router
     validate,
     getMembers
   );
+
+router
+  .route("/task")
+  .post(verifyJWT, taskValidator, validate, createTask)
+  .patch(
+    verifyJWT,
+    [...taskValidator, ...taskIdValidator],
+    validate,
+    updateTask
+  )
+  .delete(verifyJWT, deleteTaskValidator, validate, deleteTask);
+
+router.post(
+  "/task/assign-member-to-task",
+  [verifyJWT, assignMemberToTaskValidator, validate],
+  assignMemberToTask
+);
+
+router.post(
+  "/task/remove-member-from-task",
+  [verifyJWT, assignMemberToTaskValidator, validate],
+  removeMemberFromTask
+);
+
+router.route("/task/:projectId").get(verifyJWT, getTasks);
 
 router
   .route("/:projectId")
